bc80718ddb3bf1da667b693e4abcfb05e9aeef9d
xianliangwong <xianliangwong0620@gmail.com>
2024-08-30 12:00:50 +0800

    StoreJWTTokenImplementation-28/8

---
+++ src/API/Service/AuthService.tsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { handleError } from "../../Helper/ErrorHandle";
+import { UserProfileToken } from "../../Model/User";
+
+const api = "https://localhost:7096/api";
+
+export const loginAPI = async (userName: string, passWord: string) => {
+  try {
+    const data = await axios.post<UserProfileToken>(api + "account/login", {
+      username: userName,
+      password: passWord,
+    });
+
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const registerAPI = async (
+  email: string,
+  userName: string,
+  passWord: string
+) => {
+  try {
+    const data = await axios.post<UserProfileToken>(api + "account/register", {
+      email: email,
+      username: userName,
+      password: passWord,
+    });
+
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 import NavBar from "./Compoments/NavBar/NavBar";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
       <NavBar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 }
